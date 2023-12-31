7b9330f6f603309cc50f7900a5095250d7d2c090
Brook Mao <brook.mao@mail.utoronto.ca>
2023-12-30 19:37:57 -0500

    Set up database constants and PDO

--- constants.php
+++ constants.php
@@ -8,3 +8,22 @@
 
 // Database
 const DB_NAME = "res_profile";
+
+// Tables
+const USERS_TABLE = "users";
+const PROFILES_TABLE = "profile";
+
+// Columns for USERS_TABLE
+const USER_ID_COLNAME = "user_id";
+const USER_NAME_COLNAME = "name";
+const USER_EMAIL_COLNAME = "email";
+const USER_PSWD_COLNAME = "password";
+
+// Columns for PROFILES_TABLE
+const PROFILE_ID_COLNAME = "profile_id";
+const PROFILE_USER_ID_COLNAME = "user_id";
+const PROFILE_FNAME_COLNAME = "first_name";
+const PROFILE_LNAME_COLNAME = "last_name";
+const PROFILE_EMAIL_COLNAME = "email";
+const PROFILE_HEADLINE_COLNAME = "headline";
+const PROFILE_SUMM_COLNAME = "summary";
